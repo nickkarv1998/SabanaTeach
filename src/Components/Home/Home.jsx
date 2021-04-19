7454a1ce19ed03c0b79179110b12251b8f9dc9a7
mariasaavega <mariasaavega@hotmail.com>
2021-04-19 18:06:22 -0500

    Fixing Home

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect } from 'react'
 import SearchBar from '../SearchBar/SearchBar'
 import './Home.css'
+import News from './NW.jpg';
+
 
 const Home = ({ setShowMenu }) => {
 
@@ -29,16 +31,20 @@ const Home = ({ setShowMenu }) => {
             <div className="news">
 
                 <div className="card">
+                <img className="new" src={News} alt=""/>
+                
+                <div className="bar">
+                    sgrf
+                </div>
 
-                <blockquote class="twitter-tweet"><p lang="es" dir="ltr">Los invitamos al evento &quot;Innovar desde donde estés&quot; organizado por nuestro partner Google for Education, para seguir conversando sobre el futuro de la educación!<br />. <br />Cuándo: miércoles 24 de marzo, 18 hs Argentina<br />Info e inscripción: <a href="https://t.co/VoE25eDXLW">https://t.co/VoE25eDXLW</a> <a href="https://t.co/ZRcRCllvzW">pic.twitter.com/ZRcRCllvzW</a></p>&mdash; Educacion Tecnologica (@edu_tecnologica) <a href="https://twitter.com/edu_tecnologica/status/1374052643060977670?ref_src=twsrc%5Etfw">March 22, 2021</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>              
-                  </div>
-                                <div className="card">
-                                    ggtregtr
+                </div>
+                <div className="card">
+                    ggtregtr
                 </div>
 
             </div>
 
-               
+
         </div>
     )
 }
