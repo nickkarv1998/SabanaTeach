d9373575801e7d05b181d91aeeebe9daf52821bf
mariasaavega <mariasaavega@hotmail.com>
2021-06-01 12:55:11 -0500

    Error Handling

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -6,21 +6,44 @@ import axios from '../../Instances/axiosInstance.js'
 import { connect } from "react-redux";
 
 class Login extends Component {
+
+
     state = {
         email: '',
         rawPassword: ''
     }
 
     render() {
+
+        if (this.props.error) {
+            return (
+                <div>
+                    <h1 className={styles.principal}>SabanaTeach</h1>
+                    <img className={styles.image} src={Logo} alt="" />
+                    <form className={styles.login}>
+                        <p className="error"> Datos no validos!!</p>
+                        <input type="text" name="email" className={styles.campo} placeholder="Email"
+                            value={this.state.email} onChange={this.handleInputChange} />
+                        <input type="password" name="rawPassword" className={styles.campo} placeholder="Constraseña"
+                            value={this.state.rawPassword} onChange={this.handleInputChange} />
+
+                    </form>
+                    <button className={styles.button} onClick={this.signIn}>Iniciar Sesión</button>
+                </div>
+            )
+
+        }
         return (
             <div>
                 <h1 className={styles.principal}>SabanaTeach</h1>
                 <img className={styles.image} src={Logo} alt="" />
                 <form className={styles.login}>
+                {/* {this.props.IsError ? ( <p className="error"> Datos no validos!!</p>) : (  <p className="error"> Datos validos!!</p> )} */}
                     <input type="text" name="email" className={styles.campo} placeholder="Email"
                         value={this.state.email} onChange={this.handleInputChange} />
                     <input type="password" name="rawPassword" className={styles.campo} placeholder="Constraseña"
                         value={this.state.rawPassword} onChange={this.handleInputChange} />
+
                 </form>
                 <button className={styles.button} onClick={this.signIn}>Iniciar Sesión</button>
             </div>
@@ -39,13 +62,16 @@ class Login extends Component {
             rawPassword: this.state.rawPassword
         }).then(res => {
             localStorage.setItem("userId", res.data.id)
-     //       alert("Ingreso exitoso")
+            //       alert("Ingreso exitoso")
             this.props.onLogin(res.data)
             this.props.history.push('/Home')
         }).catch(_ => {
             alert("Información incorrecta")
+            this.props.onError()
+
         })
     }
+
 
 }
 
@@ -60,7 +86,20 @@ const mapDispatchToProps = (dispatch) => {
                     email: user.email,
                 }
             }),
+            onError: () =>
+            dispatch({
+                type: "ERROR",
+            }),
+
     }
 }
 
-export default connect(null, mapDispatchToProps)(withRouter(Login))
+const mapStateToProps = (state) => {
+    return {
+        logged: state.sesionStore.IsUserLoggedIn,
+        error: state.sesionStore.error,
+    };
+};
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(withRouter(Login))
